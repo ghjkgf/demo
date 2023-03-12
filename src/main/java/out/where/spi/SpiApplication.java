aaca91a51d638f6c62a9086d8e68d3352bbd9f1f
ghjkgf <zhaosenlin2018@gmail.com>
2023-03-12 16:41:54 +0800

    2222

--- src/main/java/out/where/spi/SpiApplication.java
+++ src/main/java/out/where/spi/SpiApplication.java
@@ -1,18 +1,13 @@
 package out.where.spi;
 
-import bilibili.Demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpiApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SpiApplication.class, args);
-        Demo bean = context.getBean(Demo.class);
-        System.out.println("why");
-
+        SpringApplication.run(SpiApplication.class, args);
     }
 
 }
